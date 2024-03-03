53fee5022b7475152a9b19cc3c4a1c45305203ac
Soobeen <110775069+5oob33n@users.noreply.github.com>
2024-03-03 18:26:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ function togglePopup() {
     popup.classList.toggle("active");
 }
 
+document.addEventListener('DOMContentLoaded', function() {
+    const menuIcon = document.querySelector('.menu-icon');
+    const menu = document.querySelector('.menu');
+  
+    menuIcon.addEventListener('click', function() {
+      menu.classList.toggle('active');
+      menuIcon.classList.toggle('active');
+    });
+  });
+  
